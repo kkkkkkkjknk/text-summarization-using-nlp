50a77cba3df1682ce8045b81ab6ee7c34a524b3c
Nitish <madambakamn@gmail.com>
2025-04-17 21:30:53 +0530

    Fix: Set Python 3.10 and pin compatible spaCy dependencies

--- helper.py
+++ helper.py
@@ -12,8 +12,13 @@ import streamlit as st
 import random
 import spacy
 
-nlp= spacy.load("en_core_web_sm")
-#nlp= spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 stopwords = list(STOP_WORDS)
 punctuation = punctuation + "\n"
 
